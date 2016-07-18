98344ab569b6c5050fb456b5fae1420801ff7d7d
Tobias Hennig <tobias.hennig1@gmail.com>
2016-07-18 14:27:44 +0200

    Show all exploded time bombs, fix #4

--- README.md
+++ README.md
@@ -65,6 +65,7 @@ grunt.initConfig({
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
 
 ## Release History
+* [0.1.7] Show all exploded time bombs, fix #4
 * [0.1.6] Fix #5
 * [0.1.5] Fix #6
 * [0.1.4] Update dev-/dependencies
--- tasks/time_bomb.js
+++ tasks/time_bomb.js
@@ -16,13 +16,20 @@ module.exports = function (grunt) {
     bd.parse(this.filesSrc)
     var timers = bd.timers.get()
     var bombs = bd.timers.bombs()
+    var message = ''
 
     if (timers.length === 0) {
       grunt.log.write('No timers found.')
     } else if (timers.length > 0 && bombs.length === 0) {
       grunt.log.ok('No exploded time bombs found.')
     } else if (bombs.length > 0) {
-      grunt.fail.warn('Exploded time bomb found in file: ' + bombs[0].file)
+      bombs.forEach(function (bomb) {
+        message = 'Exploded time bomb found in file: ' + bomb.file
+        if (bomb.text) {
+          message += ' - ' + bomb.text
+        }
+        grunt.fail.warn(message)
+      }, this)
     }
   })
 }
